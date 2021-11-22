bf8f19e130d0f2411407278a5c9b12cac4aab29a
ramonidias <ramonidemorais@gmail.com>
2021-11-22 15:53:02 -0300

    Colocar variaveis no meio de um texto.

---
+++ Proj_JS/5-AtribuicaoVariaveis.js
@@ -0,0 +1,19 @@
+
+console.log("\n","# Trabalhando com atribuições de variaveis #","\n")
+
+var nome = "Ramoni";
+var sobrenome = "Dias"
+
+//Juntar variaveis strings ou valores
+console.log(nome + " " + sobrenome,"\n");
+//ou
+//Tem q usar crazi em vez de aspas p/ dar certo
+console.log(`Meu nome é ${nome} ${sobrenome}`,"\n")
+
+nome = nome + sobrenome;
+
+let idade;
+idade = 26;
+idade = idade + 1;
+console.log(idade);
+
