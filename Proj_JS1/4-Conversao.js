9d7d89c0ca580ec2f450557ffec7994b738d0d05
ramonidias <ramonidemorais@gmail.com>
2021-12-02 11:32:24 -0300

    Conversão de valores.

---
+++ Proj_JS1/4-Conversao.js
@@ -0,0 +1,16 @@
+
+console.log("\n","Soma String:","2" + "2","\n");
+
+//Converter dados
+console.log(parseInt( "2"));
+
+//Ele faz a conversão e a divisão, por entender o sinal 
+console.log("10" / "2");
+
+//NaN = Não consegue converter pq é string
+console.log("Ramoni" / "2");
+
+//Ponto flutuando com ponto"."", não com vírgula.
+console.log(6.5)
+
+
