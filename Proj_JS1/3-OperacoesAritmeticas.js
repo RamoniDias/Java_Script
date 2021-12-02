e1d6509628f04c20b5e316fe0ac4cf982cec0a25
ramonidias <ramonidemorais@gmail.com>
2021-12-02 11:32:15 -0300

    Operações matematicas.

---
+++ Proj_JS1/3-OperacoesAritmeticas.js
@@ -0,0 +1,12 @@
+
+console.log("\n console.log: Operações Aritméticas.\n");
+
+console.log(" Multiplicação:",10 * 10,"\n");
+
+console.log(" Adição:",10 + 10,"\n");
+
+console.log(" Subtração:",10 - 10,"\n");
+
+console.log(" Junção:","Ano" + 2020,"\n");
+
+console.log(" Soma Straing:","2" + "2","\n");
