74d9020c7f36907d99026889d32ebd1bb966e19f
ramonidias <ramonidemorais@gmail.com>
2021-12-20 17:11:17 -0300

    Class Conta Corrente v.01

--- Proj_JS2/ContaCorrente.js
+++ Proj_JS2/ContaCorrente.js
@@ -1,10 +1,34 @@
 
 export class ContaCorrente {
-  agencia;
   cliente;
-
-
+  agencia;
   saldo; //Atributo Privado, para isso usa-se o _ outline
+
+  static numeroDeContas = 0; //Onde vc alterar vai alterar em todo projeto
+
+  constructor(cliente, agencia, saldo) {
+    this.cliente = cliente;
+    this.agencia = agencia;
+    this.saldo = saldo;
+
+    ContaCorrente.numeroDeContas += 1; // Declarando contador Statico.
+  }
+
+  set cliente(novoValor) {
+    //Proteger atributo// Método de Atribuição
+    if (novoValor instanceof Cliente) {
+      this.cliente = novoValor;
+    }
+  }
+
+  get cliente() {
+    //Pegar atributo // Método de Acesso
+    return this.cliente; //Encapsulamento maior privacidade - somente leitura
+  }
+
+  get saldo() {
+    return this.saldo;
+  }
 
   //Função ou Método recebe a variavel que contém um valor
   sacar(valor) {
@@ -17,9 +41,14 @@ export class ContaCorrente {
   }
 
   depositar(valor) {
-    if (valor > 0) {
-      this.saldo += valor;
-      return valor;
+    if (valor <= 0) {
+      return;
     }
+    this.saldo += valor;
+  }
+
+  transferir(valor, conta) {
+    const valorSacado = this.sacar(valor);
+    conta.depositar(valorSacado);
   }
 }
