4945cdfa905e00a50aa64ca27a2de06308a652af
ramonidias <ramonidemorais@gmail.com>
2021-12-20 17:11:46 -0300

    Corpo de Desenvolvimento.

--- Proj_JS2/index.js
+++ Proj_JS2/index.js
@@ -1,29 +1,44 @@
-
-import {Cliente} from "./Cliente.js";//Importando esta Classe deste arquivo
+import { Cliente } from "./Cliente.js"; //Importando esta Classe deste arquivo
 import { ContaCorrente } from "./ContaCorrente.js";
 
-const cliente1 = new Cliente(); 
-cliente1.nome    =   "Ricardo";
-cliente1.cpf     = 11111111100;
+const cliente1 = new Cliente();
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11111111100;
 
 const cliente2 = new Cliente();
-cliente2.nome    = "Alice";
-cliente2.cpf     = 22222222200;
+cliente2.nome = "Alice";
+cliente2.cpf = 22222222200;
 
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.agencia = "1001"; //implementar valor errado
-contaCorrenteRicardo.saldo   =      0;
-contaCorrenteRicardo.cliente = cliente1; //Pegando os atributos de outra Classe
+//Antes:
+//const contaCorrenteRicardo = new ContaCorrente();
+//contaCorrenteRicardo.cliente = cliente1;
+//contaCorrenteRicardo.agencia = "1001"; //implementar valor errado
+//contaCorrenteRicardo.saldo = 0;
+
+//Depois:
+const contaCorrenteRicardo = new ContaCorrente("Ricardo", 11111111100, 0);
+
+
+// // contaCorrenteRicardo.cliente = cliente1; //Pegando os atributos de outra Classe
 
 //contaCorrenteRicardo.depositar(100);   // Depósito Positivo
 //contaCorrenteRicardo.depositar(100);   // implementar valor CERTO
-//contaCorrenteRicardo.depositar(100); 
+//contaCorrenteRicardo.depositar(100);
 //console.log("\n","Saldo:",contaCorrenteRicardo.saldo)
 
 //const valorSacado = contaCorrenteRicardo.sacar(50); //Para mostrar valor do saque, se criou uma variavel para mostrar
 //console.log("\n","Valor Sacado:",valorSacado);
 
+//console.log("\n",contaCorrenteRicardo, "\n");
 
-console.log("\n",contaCorrenteRicardo, "\n");   
+contaCorrenteRicardo.depositar(500);
+
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente2;
+conta2.agencia = 102;
+conta2.saldo = 0;
 
 
+contaCorrenteRicardo.transferir(150, conta2);
+
+console.log("\n", ContaCorrente.numeroDeContas);
