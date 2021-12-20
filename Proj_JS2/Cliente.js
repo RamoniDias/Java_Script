6bbfc3d02c66476535763c512dcdf3f6c1d9ddfa
ramonidias <ramonidemorais@gmail.com>
2021-12-20 17:10:45 -0300

    Class Cliente v.01

--- Proj_JS2/Cliente.js
+++ Proj_JS2/Cliente.js
@@ -1,5 +1,23 @@
 
-export class Cliente {// Para poder usar a Classe em outro arquivo temos q exporta la
-    nome;
-    cpf;
+export class Cliente {
+  // Para poder usar a Classe em outro arquivo temos q exporta la
+  nome;
+  cpf;
+
+  get cpf() {// Não do nenhum acessor para que ele possa alterar esse CPF
+    return this.cpf;
+  }
+
+  constructor(nome, cpf) {
+    this.nome = nome;
+    this.cpf = cpf;
+  }
+
+
+
+
+
+
 }
+  
+
